e7ad77fcd4d67fa02afac4f81c2cd0873cfac775
vkyscen <vickyeditz@gmail.com>
2020-05-07 15:10:45 +0530

    Completed demo project

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+  cognito: {
+    userPoolId: "ap-south-1_iM6CfhzRK", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "5t03sk9saftcpq6saorhh4ljfd", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "ap-south-1", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "https://ip4ym8fxzc.execute-api.ap-south-1.amazonaws.com/prod", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+  },
+};
